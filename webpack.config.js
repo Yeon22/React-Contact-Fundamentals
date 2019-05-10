c47acf1ee6f0a44473a39f08eadc58151f92843c
Suyeon <bsuyeon16@gmail.com>
2019-05-10 15:53:20 +0900

    create React App. init configurations.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack'); // webpack module을 불러옴
+
+module.exports = {
+  entry: './src/index.js', // 시작 위치
+  output: { // bundle된 하나의 파일을 /public/bundle.js라는 이름으로 저장
+    path: __dirname + '/public/',
+    filename: 'bundle.js',
+  },
+  devServer: { // 개발 서버 설정
+    hot: true, // 파일 수정할 때마다 reloading
+    inline: true, // hot reloading에 필요한 webpack dev server 클라이언트를 bundle에 같이 넣어줌
+    host: '0.0.0.0', // server를 listen할 주소. 기본 값은 localhost
+    port: 4000, // 개발 서버 port
+    contentBase: __dirname + '/public/', // index 파일의 위치
+  },
+  module: { // loader를 이용해 JSX, CSS, SASS 등을 일반 자바스크립트 형식으로 변환
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        },
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
